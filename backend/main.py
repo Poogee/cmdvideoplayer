e2cda11b3d9a15fadeb7fae0c4856f5be0dc384d
shevlfs <shevl.fs@gmail.com>
2022-08-14 20:34:15 +0300

    eto baza

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import Flask, redirect, url_for, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import make_response
 from flask import request, flash
 import requests
 from flask_sqlalchemy import SQLAlchemy
-from data import database, mail, secretKey
+#from data import database, mail, secretKey
 import re
 from flask import render_template
 from sqlalchemy.orm.attributes import flag_modified
@@ -13,3 +14,13 @@ from flask_mail import Mail, Message
 from itsdangerous import URLSafeTimedSerializer
 from flask_login import login_user, login_required, current_user, LoginManager, logout_user
 import base64
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def handshake():
+    return "HellO!"
+
+if __name__ == '__main__':
+   app.run()
