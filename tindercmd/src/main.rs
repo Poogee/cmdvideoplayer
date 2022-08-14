30742188355b34b937979ecec51e9bbe39421052
shevlfs <shevl.fs@gmail.com>
2022-08-14 22:07:04 +0300

    flask meets rust

--- tindercmd/src/main.rs
+++ tindercmd/src/main.rs
@@ -1,12 +1,17 @@
 use std::collections::HashMap;
 
 
+fn main(){
+    handshake();
+}
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+
+async fn handshake() -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
     let resp = reqwest::get("http://127.0.0.1:5000/")
         .await?
         .json::<HashMap<String, String>>()
         .await?;
     println!("{:#?}", resp);
-    Ok(())
+    Ok(resp)
 }
